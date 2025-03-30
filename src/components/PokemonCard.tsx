5499f80ce4bfb139c5e2d6cc4c9b809c615cfe94
Manon Guerrero <manon.guerrero.33@hotmail.fr>
2025-03-31 01:15:42 +0200

    Update PokemonCard to show ??? when image is missing

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import React from "react";
+
+const pokemonList = [
+  {
+    name: "kyogre",
+    imgSrc: "images/kyogre.png",
+    type: "Water",
+    height: "2.5m",
+    weight: "352kg"
+  },
+  {
+    name: "bulbasaur",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 const PokemonCard: React.FC = () => {
+  const pokemon = pokemonList[2];
+
   return (
     <figure>
-      <img src="images/kyogre.png" alt="Kyogre" />
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
       <figcaption>
-        <h2>Kyogre</h2>
-        <p>Type: Water</p>
-        <p>Height: 2.5m</p>
-        <p>Weight: 352kg</p>
+        <h2>{pokemon.name}</h2>
+        <p>Type: {pokemon.type}</p>
+        <p>Height: {pokemon.height}</p>
+        <p>Weight: {pokemon.weight}</p>
       </figcaption>
     </figure>
   );
